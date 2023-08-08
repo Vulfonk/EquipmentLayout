3c4a64746ffbe09b4fb6aad0c5160be9f38b07b6
Vulfonk <podkalyuk-dima@bk.ru>
2023-08-08 22:54:25 +0500

    Revert "исправил переключение между шаблонами и вылет при создании нового"
    This reverts commit e93081a5374904a3c29a97f0dfc912165744c961.

--- EquipmentLayout/ViewModels/MainWindowViewModel.cs
+++ EquipmentLayout/ViewModels/MainWindowViewModel.cs
@@ -103,8 +103,8 @@ namespace EquipmentLayout.ViewModels
                 (x) => x.Height)
             };
 
-            this.Properties = new ObservableCollection<Property<DeviceTemplateViewModel>>(properties);
             OnPropertyChanged(nameof(Properties));
+            this.Properties = new ObservableCollection<Property<DeviceTemplateViewModel>>(properties);
         }
 
         public ObservableCollection<Property<DeviceTemplateViewModel>> Properties { get; set; }
@@ -178,15 +178,7 @@ namespace EquipmentLayout.ViewModels
 
         private void AddTemplateCommand_Executed()
         {
-            if(this.SelectedDeviceTemplate != null)
-                this.DeviceTemplateViewModels.Add(this.SelectedDeviceTemplate.Clone());
-            else
-            {
-                var template2 = new DeviceTemplate(200, 100, "Name");
-                var vm_template2 = new DeviceTemplateViewModel(template2);
-                this.DeviceTemplateViewModels.Add(vm_template2);
-            }
-
+            this.DeviceTemplateViewModels.Add(this.SelectedDeviceTemplate.Clone());
 
         }
     }
